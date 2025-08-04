a951b1c55e3eb53605dd940c86065a53619d066e
sanjanaastoti05 <151532477+sanjanaastoti05@users.noreply.github.com>
2025-08-04 10:14:42 +0530

    Add files via upload

---
+++ Lab4.java
@@ -0,0 +1,16 @@
+package Day1Prrms;
+
+public class Lab4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Person Details:");
+		System.out.println("---------------");
+		System.out.println("First Name : Divya");
+		System.out.println("Last Name : Bharathi");
+		System.out.println("Gender : F");
+		System.out.println("Age : 20");
+		System.out.println("Weight : 85.55");
+	}
+
+}
